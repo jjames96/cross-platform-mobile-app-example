6a7a02ca8d695521aabd08dca203c01d52e72797
Joe <joejamesme0000@gmail.com>
2020-11-05 18:59:11 +0000

    Add basic shared create account

---
+++ shared/src/commonMain/kotlin/me/joejames/kmmapp/shared/model/account/AccountDetails.kt
@@ -0,0 +1,7 @@
+package me.joejames.kmmapp.shared.model.account
+
+data class AccountDetails(
+    val username: String,
+    val password: String,
+    val confirmPassword: String
+)
--- shared/src/commonMain/kotlin/me/joejames/kmmapp/shared/screen/BaseView.kt
+++ shared/src/commonMain/kotlin/me/joejames/kmmapp/shared/screen/BaseView.kt
@@ -2,4 +2,5 @@ package me.joejames.kmmapp.shared.screen
 
 interface BaseView {
     fun showAlert(message: String)
+    fun closeSelf()
 }
