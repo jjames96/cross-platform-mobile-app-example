8a7a55ba963888e2a631a898eaa3ff9391070493
Joe <joejames1996@hotmail.com>
2021-02-25 16:05:21 +0000

    Handle new alerts in iOS + Fix scheme

--- iosApp/iosApp/Screen/BaseViewController.swift
+++ iosApp/iosApp/Screen/BaseViewController.swift
@@ -2,6 +2,33 @@ import shared
 import UIKit
 
 class BaseViewController : UIViewController, BaseView {
+    func closeSelf() {
+        // TODO
+    }
+    
+    func showAlert(message: String, positiveAction: AlertAction, negativeAction: AlertAction?) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addAction(
+            UIAlertAction(
+                title: positiveAction.actionText,
+                style: .default,
+                handler: { _ in positiveAction.actionCallback() }
+            )
+        )
+        
+        if let action = negativeAction {
+            alertController.addAction(
+                UIAlertAction(
+                    title: action.actionText,
+                    style: .cancel,
+                    handler: { _ in action.actionCallback() }
+                )
+            )
+        }
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     func showAlert(message: String) {
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         self.present(alertController, animated: true, completion: nil)
